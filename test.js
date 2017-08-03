c45e3addb77536a89a60005033901708b91d33a0
Francesco Banconi <francesco.banconi@canonical.com>
2017-08-03 17:56:27 +0200

    Add tests for deepFreeze.

--- test.js
+++ test.js
@@ -6,9 +6,34 @@ const test = require('tape');
 // Define the property name for the shape information.
 const SHAPE = '__shape__';
 
-test('reshapeFunc', t => {
+test('deepFreeze', t => {
+  t.test('deeply freezes objects', t => {
+    const obj = {
+      array: [1, 2, 3],
+      obj: {array: [], func: () => {}},
+      func: () => {},
+      map: new Map(),
+      str: 'exterminate!',
+      number: 42
+    };
+    const frozen = shapeup.deepFreeze(obj);
+    const checkFrozen = obj => t.ok(Object.isFrozen(obj), JSON.stringify(obj));
+    checkFrozen(obj);
+    checkFrozen(obj.array);
+    checkFrozen(obj.obj);
+    checkFrozen(obj.obj.array);
+    checkFrozen(obj.obj.func);
+    checkFrozen(obj.obj);
+    checkFrozen(obj.func);
+    checkFrozen(obj.map);
+    checkFrozen(obj.str);
+    checkFrozen(obj.number);
+    t.end();
+  });
+});
 
-  t.test('returns null', st => {
+test('reshapeFunc', t => {
+  t.test('returns null', t => {
     t.equal(shapeup.reshapeFunc(), null);
     t.end();
   });
@@ -18,5 +43,4 @@ test('reshapeFunc', t => {
     t.equal(reshape.constructor.name, 'Reshape');
     t.end();
   });
-
 });
